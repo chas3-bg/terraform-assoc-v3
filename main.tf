1edd1383f9dca1b554ad633ab78f980ea90bbdcf
Kiril Kolessarsky <kiril.kolessarsky@accenture.com>
2025-03-28 17:53:22 +0200

    single server

--- main.tf
+++ main.tf
@@ -10,7 +10,24 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-1"
+  region = "eu-central-1"
+}
+
+variable "port" {
+  description = "Port for the application"
+  type = number
+  default = 8080
+  
+}
+
+
+output "PublicIP" {
+  value = aws_instance.test_server.public_ip 
+}
+
+output "PublicDNS" {
+  value = aws_instance.test_server.public_dns
+  
 }
 
 data "aws_ami" "fedora41" {
@@ -23,17 +40,48 @@ data "aws_ami" "fedora41" {
 }
 
 resource "aws_instance" "test_server" {
-  ami           = data.aws_ami.fedora41.id
-  instance_type = "t2.micro"
-  availability_zone = "eu-west-1a"
+  ami               = data.aws_ami.fedora41.id
+  instance_type     = "t2.micro"
+  availability_zone = "eu-central-1a"
+  vpc_security_group_ids = ["aws_security_group.defaultsg.id"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello from terraformed server" > index.html
+              nohup busibox httpd -f -p "${var.port}" &
+              EOF
 
   tags = {
     Name = "Test server"
   }
 }
 
+resource "aws_security_group" "defaultsg" {
+  name        = "defaultSG"
+  description = "Allow 8080/tcp"
+
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = var.port
+    to_port     = var.port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+
+
 resource "aws_ebs_volume" "data" {
-  availability_zone = "eu-west-1a"
+  availability_zone = "eu-central-1a"
   size              = 1
   encrypted         = true
   type              = "gp3"
