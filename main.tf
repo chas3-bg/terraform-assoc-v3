35aa4d81c41f2f4fa74614a1469557c048c8da9d
Kiril Kolessarsky <kiril.kolessarsky@accenture.com>
2025-03-28 18:07:33 +0200

    autoscaling -1

--- main.tf
+++ main.tf
@@ -22,25 +22,64 @@ data "aws_ami" "fedora41" {
   }
 }
 
-resource "aws_instance" "test_server" {
-  ami           = data.aws_ami.fedora41.id
-  instance_type = "t2.micro"
-  availability_zone = "eu-west-1a"
+data "aws_availability_zones" "all" {}
 
-  tags = {
-    Name = "Test server"
+variable "app_port" {
+  default = 8080
+  
+}
+
+variable "allow_all" {
+  default = "0.0.0.0/0"
+  
+}
+resource "aws_launch_configuration" "test_server" {
+  image_id           = data.aws_ami.fedora41.id
+  instance_type = "t2.micro"
+  security_groups = ["aws_security_group.defaultsg.id"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello from terraformed server" > index.html
+              nohup busybox httpd -f -p "${var.app_port}"
+              EOF
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
-resource "aws_ebs_volume" "data" {
-  availability_zone = "eu-west-1a"
-  size              = 1
-  encrypted         = true
-  type              = "gp3"
+
+resource "aws_autoscaling_group" "app_servers" {
+  launch_configuration = aws_launch_configuration.test_server.id
+  min_size = 2
+  max_size = 10
+  availability_zones = data.aws_availability_zones.all.names
+
+  tag {
+    key = "name"
+    value = "terraform v3"
+    propagate_at_launch = true
+  }
+  
+}
+resource "aws_security_group" "defaultsg" {
+  name        = "defaultsg"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = var.app_port
+    to_port     = var.app_port
+    protocol    = "tcp"
+    cidr_blocks = var.allow_all
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.allow_all]
+  }
+
 }
 
-resource "aws_volume_attachment" "ebs_attach" {
-  device_name = "/dev/sdb"
-  volume_id   = aws_ebs_volume.data.id
-  instance_id = aws_instance.test_server.id
-}
+#TODO: LB
